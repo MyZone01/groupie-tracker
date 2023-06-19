52ab10e39beb30d41eac74d6f69e973b7b99f374
bakseck <seck.bakar@ugb.edu.sn>
2023-06-19 15:18:33 +0000

    Make_Responsive_Cards

--- lib/handlers/artists.go
+++ lib/handlers/artists.go
@@ -13,7 +13,11 @@ import (
 func ArtistList(res http.ResponseWriter, req *http.Request) {
 	if utils.ValidateRequest(req, res, "/artists/", http.MethodGet) {
 		url := "https://groupietrackers.herokuapp.com/api/artists"
-		data := utils.GetAPI(url)
+		data, err := utils.GetAPI(url)
+		if err != nil {
+			utils.RenderPage("500", nil, res)
+			return
+		}
 		var artists []models.ArtistModel
 		json.Unmarshal(data, &artists)
 
@@ -29,16 +33,35 @@ func ArtistInfos(res http.ResponseWriter, req *http.Request) {
 		idArtist := path.Base(req.URL.Path)
 
 		artistURL := "https://groupietrackers.herokuapp.com/api/artists/" + idArtist
-		data1 := utils.GetAPI(artistURL)
+		data1, err := utils.GetAPI(artistURL)
+		if err != nil {
+			utils.RenderPage("500", nil, nil)
+			return
+		}
+
 		var artist models.ArtistModel
-		json.Unmarshal(data1, &artist)
+		err = json.Unmarshal(data1, &artist)
+		if err != nil {
+			utils.RenderPage("500", nil, res)
+			return
+		}
+
 		if artist.Id != 0 {
 			artist.FirstAlbum = utils.FormatDates(artist.FirstAlbum)
 
 			relationURL := "https://groupietrackers.herokuapp.com/api/relation/" + idArtist
-			data := utils.GetAPI(relationURL)
+			data, err := utils.GetAPI(relationURL)
+			if err != nil {
+				utils.RenderPage("500", nil, res)
+				return
+			}
+
 			var _relation models.RelationModel
-			json.Unmarshal(data, &_relation)
+			err = json.Unmarshal(data, &_relation)
+			if err != nil {
+				utils.RenderPage("500", nil, res)
+				return
+			}
 
 			locations := utils.FormatLocations(_relation)
 
@@ -54,3 +77,4 @@ func ArtistInfos(res http.ResponseWriter, req *http.Request) {
 		}
 	}
 }
+
